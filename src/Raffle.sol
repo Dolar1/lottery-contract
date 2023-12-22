665d5ae3aa226d04f94b4f9e0f8d7ca2e215c5f6
saurabh_singh <saurabh.singh@gojek.com>
2023-12-22 22:03:45 +0530

    initial commit

---
+++ src/Raffle.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: UNLICENSED
+
+/*
+[CHECKS EFFECTS INTERACTIONS => CEI]
+CHECKS => First check for any thing in ur contract and revert asap for being gas effiecinet
+EFFECTS => making any changes to your current contract
+INTERACTIONS => it is if you wanna  interact with any other contract after
+done with compute in current contract;
+*/
+pragma solidity ^0.8.18;
+
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+
+contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    error Raffle__NotEnoughEthSent();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+    error Raffle__UpKeepNotNeeded(
+        uint256 currentBalance,
+        uint256 numPlayers,
+        uint256 raffleState
+    );
+
+    enum RaffleState {
+        OPEN,
+        CALCULATING,
+        CLOSED
+    }
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 3;
+
+    uint256 private immutable i_entrancefee;
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_gasLimit;
+    address private s_recentWinner;
+
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+    RaffleState private s_raffleState;
+
+    // Events
+    event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callBackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_entrancefee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_gasLimit = callBackGasLimit;
+
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        if (msg.value < i_entrancefee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        if (s_raffleState != RaffleState.CLOSED) {
+            revert Raffle__RaffleNotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    ) public view returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool hasTimePassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
+        bool hasPlayers = s_players.length > 0;
+        bool hasBalance = address(this).balance > 0;
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        upkeepNeeded = (hasTimePassed && hasPlayers && hasBalance && isOpen);
+        return (upkeepNeeded, "0x0");
+    }
+
+    function performUpkeep(bytes calldata performData) external {
+        (bool upkeepNeeded, ) = checkUpkeep(performData);
+        if (!upkeepNeeded) {
+            revert Raffle__UpKeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
+
+        s_raffleState = RaffleState.CALCULATING;
+        i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_gasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 /*_requestId  */,
+        uint256[] memory _randomWords
+    ) internal override {
+        uint256 indexOfWinner = _randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(winner);
+
+        (bool suceess, ) = winner.call{value: address(this).balance}("");
+        if (!suceess) {
+            revert Raffle__TransferFailed();
+        }
+    }
+
+    function pickOwner() public {}
+
+    // getter functions
+    function getEntranceFee() external view returns (uint256) {
+        return i_entrancefee;
+    }
+}
